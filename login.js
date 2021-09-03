187695a25f03c7cdb87b1ae0f5e5b539b88e9c6b
CLL612 <62069895+CLL612@users.noreply.github.com>
2021-09-04 00:56:02 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,20 +1,17 @@
-/**
- * Created by Kay on 2016/3/8.
- */
 function login() {
 
     var username = document.getElementById("username");
     var pass = document.getElementById("password");
 
-    if (username.value == "cll") {
+    if (username.value == "") {
 
         alert("请输入用户名");
 
-    } else if (pass.value  == "612") {
+    } else if (pass.value  == "") {
 
         alert("请输入密码");
 
-    } else if(username.value == "admin" && pass.value == "123456"){
+    } else if(username.value == "admin" && pass.value == "admin"){
 
         window.location.href="main.html";
 
